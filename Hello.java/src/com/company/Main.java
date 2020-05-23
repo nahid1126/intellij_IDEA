8fd5891548eb8920430f0b143861a1adfd8ef5f4
nahid <nahidnub1126@gmail.com>
2020-05-23 23:35:13 +0600

    Created a branch

--- Hello.java/src/com/company/Main.java
+++ Hello.java/src/com/company/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("HELLO!!");
-        print("Nahid Hasan");
+        Display("Nahid Hasan");
     }
-    public static void print(String name){
-        System.out.println("Welcome "+name);
+    public static void Display(String name){
+        System.out.println("Bye Bye "+name);
     }
 }
