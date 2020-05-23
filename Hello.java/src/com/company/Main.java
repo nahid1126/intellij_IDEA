4b5842c242573328e79d11bc8187e78e6e4a7359
nahid <nahidnub1126@gmail.com>
2020-05-23 23:42:41 +0600

    Created a new method called add

--- Hello.java/src/com/company/Main.java
+++ Hello.java/src/com/company/Main.java
@@ -6,8 +6,13 @@ public class Main {
 	// write your code here
         System.out.println("HELLO!!");
         Display("Nahid Hasan");
+        int a=add(7,5);
+        System.out.println("The sum is = "+a);
     }
     public static void Display(String name){
         System.out.println("Bye Bye "+name);
+    }
+    public static int add(int a,int b){
+        return a+b;
     }
 }
