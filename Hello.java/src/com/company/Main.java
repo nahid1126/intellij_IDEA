bacf98410e02f0e9c3ea89b81f1aca3cd19fc4de
nahid <nahidnub1126@gmail.com>
2020-05-23 23:26:06 +0600

    Created a new method

--- Hello.java/src/com/company/Main.java
+++ Hello.java/src/com/company/Main.java
@@ -5,5 +5,9 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("HELLO!!");
+        print("Nahid Hasan");
+    }
+    public static void print(String name){
+        System.out.println("Welcome "+name);
     }
 }
